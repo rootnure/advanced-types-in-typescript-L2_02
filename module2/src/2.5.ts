af05394314f5713314fc764101c16638519738f8
rootnure <nur.diu.2791@gmail.com>
2024-06-12 09:08:58 +0600

    Tuple in Function with Generic

--- module2/src/2.5.ts
+++ module2/src/2.5.ts
@@ -21,10 +21,30 @@
   const resGenericObj = createArrayWithGeneric<User>({ id: 22156, name: 'Mr. Rasib' });
 
 
+  // Tuple
+  const createArrayWithTuple = <T, X>(param1: T, param2: X): [T, X] => {
+    return [param1, param2];
+  };
+
+
+  const response1 = createArrayWithTuple<string, number>('BD', 222);
+  const responseGeneric = createArrayWithTuple<string, { name: string }>('Bangladesh', { name: 'Asia' });
+
+
+  const addCourseToStudent = <T>(student: T): object => {
+    const course = 'Next Level Web Development';
+
+    return {
+      ...student,
+      course
+    }
+  }
+
+  const updatedStudent = addCourseToStudent<{ name: string; batch: number, email: string, devType: string }>({ name: 'Nur', batch: 8, email: 'nur@mail.com', devType: 'JLWD' });
 
 
 
 
-
+  console.log({ res1, resGeneric, resGenericObj, response1, responseGeneric, updatedStudent });
   //
 }
