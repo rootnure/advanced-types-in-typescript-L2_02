bd8f3e7b746951c181995e73872591ed763e8f04
rootnure <nur.diu.2791@gmail.com>
2024-06-12 07:52:58 +0600

    Intro to Generics in TS

--- module2/src/2.3.ts
+++ module2/src/2.3.ts
@@ -1,8 +1,22 @@
 {
   // generic type
 
-  const rollNumbers: number[] = [2, 9, 4, 8];
+  // type GenericArray<param> = Array<param>;
+  type GenericArray<T> = Array<T>; // industrialized code
 
+  // const rollNumbers: number[] = [2, 9, 4, 8];
+  // const rollNumbers: Array<number> = [2, 9, 4, 8];
+  const rollNumbers: GenericArray<number> = [2, 9, 4, 8];
+
+  // const mentors: string[] = ['Mr.X', 'Mr. Y', 'Mr.Z'];
+  const mentors: GenericArray<string> = ['Mr.X', 'Mr. Y', 'Mr.Z'];
+
+  // const boolArray: boolean[] = [true, false, true];
+  const boolArray: GenericArray<boolean> = [true, false, true];
+
+  const add = (x: number, y: number) => x + y;
+
+  add(30, 20);
 
 
 
